73d23d6211fb1a07a841498a53b0a316deecf1b8
WinKeeper <winkeeper@mail.ru>
2021-02-28 12:27:54 +0500

    1 try

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea; //импорт класса
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Controller {
     @FXML //связываем элементы через fx:id с контроллером
@@ -15,9 +21,9 @@ public class Controller {
     TextField messageField;
     //onAction="#clickSendMessage" для текстого поля это Enter
 
-
     public void clickSendMessage() {
         if (!messageField.getText().isEmpty()) {
+            mainTextArea.appendText("Время сообщения: " + new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()) + "\n");
             mainTextArea.appendText(messageField.getText() + "\n");
             messageField.clear();
         }
